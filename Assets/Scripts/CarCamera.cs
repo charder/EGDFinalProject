f517fedbd2b7495115ffdbb1b54117746693da28
charder <chardergaming@gmail.com>
2017-03-01 21:36:10 -0500

    Work has begun on car movement. Very basic, ATM
    zoom zoom zoom

---
+++ Assets/Scripts/CarCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCamera : MonoBehaviour {
+	public GameObject car;
+	Vector3 localTransform; // position that must be maintained behind car
+	// Use this for initialization
+	void Start () {
+		localTransform = transform.position - car.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = car.transform.position - transform.forward * 18 + transform.up * 3;;
+		float tmpRot = (car.transform.localRotation.eulerAngles.y - 90) - transform.localRotation.eulerAngles.y;
+		print (tmpRot);
+		transform.Rotate (0,tmpRot,0);
+		transform.eulerAngles = new Vector3 (21, transform.eulerAngles.y, 0);
+	}
+}
---
+++ Assets/Scripts/DrivingScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingScript : MonoBehaviour {
+	public float speed;
+	float speedCurrent;
+	public float rotateSpeed;
+	float rotateCurrent;
+	// Use this for initialization
+	void Start () {
+		speedCurrent = speed;
+		rotateCurrent = rotateSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			transform.position += -1 * transform.right * Time.deltaTime * speedCurrent;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += transform.right * Time.deltaTime * speedCurrent;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0, 0, rotateCurrent*-Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0, 0, rotateCurrent*Time.deltaTime);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			rotateCurrent = rotateSpeed * 3;
+			speedCurrent = speedCurrent * 0.75f;
+		} else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			rotateCurrent = rotateSpeed;
+			speedCurrent = speedCurrent / 0.75f;
+		}
+	}
+}
