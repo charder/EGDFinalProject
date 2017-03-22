d5436e3648a0c1b8dfd121ad8fd0bc249a540610
Collin Fingar <fingac@rpi.edu>
2017-03-22 17:32:59 -0400

    Started with nodes

---
+++ Assets/Scripts/Waypoints/MissionManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour {
+
+	public GameObject endNode;
+	public GameObject[] waypointNodes;
+
+	public GameObject car;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UpdateWaypoints(){
+		
+	}
+
+	void Pathfind(){
+		
+	}
+}
