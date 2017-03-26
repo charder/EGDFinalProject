8e16f5884b6f72fca0751e31b84a2595a468c3b2
Collin Fingar <fingac@rpi.edu>
2017-03-26 15:08:26 -0400

    Welcome to the world, nav arrow

--- Assets/Scripts/Waypoints/MissionManager.cs
+++ Assets/Scripts/Waypoints/MissionManager.cs
@@ -1,42 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MissionManager : MonoBehaviour {
 
-	public GameObject endNode;
-	public GameObject[] pathNodes;
-
-	public GameObject[] waypointNodes;
+	public GameObject endGoal;
 
 	public GameObject car;
+	public GameObject navArrow;
+	private NavMeshAgent arrowAgent;
 
 	public float detectDistance = 10f;
 
+
 	// Use this for initialization
 	void Start () {
-		
+		arrowAgent = navArrow.GetComponent<NavMeshAgent> ();
+		arrowAgent.destination = endGoal.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		navArrow.transform.localPosition = new Vector3 (.35f, 0, 2.6f);
 	}
 
-	void UpdateWaypoints(){
-		
-	}
-
-	void Pathfind(){
-		
-	}
-
-	void FindClosestNode(){
-		GameObject node = waypointNodes [0];
-		float distance = Vector3.Distance (car.transform.position, node.transform.position);
-		for (int i = 1; i < waypointNodes.Length; i++) {
-			float tempDistance = Vector3.Distance (car.transform.position, waypointNodes[i].transform.position);
-
-		}
-	}
 }
