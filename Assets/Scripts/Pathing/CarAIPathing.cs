343cd1f5721bc46603acf0eb0dcfc0f3c2eb0233
charder <chardergaming@gmail.com>
2017-03-28 19:12:55 -0400

    TWO CARS?????? Longer path too!
    It's the future of driving! Cars that barely follow laws and can turn on a dime. Magic!

--- Assets/Scripts/Pathing/CarAIPathing.cs
+++ Assets/Scripts/Pathing/CarAIPathing.cs
@@ -7,19 +7,18 @@ public class CarAIPathing : MonoBehaviour {
 	NavMeshAgent agent;
 	public GameObject pathObject; //object that holds a path this car will follow
 	List<Transform> pathPoints; //transforms of a path defined by pathObject
-	int currentPoint; //current point # in its path
+	public int currentPoint; //current point # in its path
 	// Use this for initialization
 	void Start () {
 		pathPoints = pathObject.GetComponent<CarAIPath> ().path_objects;
 		agent = GetComponent<NavMeshAgent> ();
 		//agent.updatePosition = false;
 		//agent.updateRotation = false;
-		currentPoint = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (agent.remainingDistance < 0.5f) {
+		if (agent.remainingDistance < 1f) {
 			if (currentPoint + 1 == pathPoints.Count) {
 				currentPoint = 0;
 			} else {
