1e7c3e8fb0f3db4069aee2e95325c279ae253ed4
charder <chardergaming@gmail.com>
2017-03-28 17:31:38 -0400

    I have a car, and it paths. WOW!
    Relax, this is just the beginning. Nothing special.

---
+++ Assets/Scripts/Pathing/CarAIPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAIPath : MonoBehaviour {
+	public Color raycolor = Color.white;
+	public List<Transform> path_objects = new List<Transform>();
+	Transform[] theArray;
+
+	void OnDrawGizmos() {
+		Gizmos.color = raycolor;
+		theArray = GetComponentsInChildren<Transform> ();
+		path_objects.Clear ();
+
+		foreach (Transform path_object in theArray) {
+			if (path_object != this.transform) {
+				path_objects.Add (path_object);
+			}
+		}
+
+		for (int i = 0; i < path_objects.Count; i++) {
+			Vector3 position = path_objects [i].position;
+			if (i > 0) {
+				Vector3 previous = path_objects [i - 1].position;
+				Gizmos.DrawLine (previous, position);
+				if (i == path_objects.Count - 1) {
+					Gizmos.DrawLine(position,path_objects[0].position);
+				}
+				Gizmos.DrawWireSphere (position, 1);
+			} else {
+				Gizmos.DrawWireSphere (position, 3); //path start
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Pathing/CarAIPathing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarAIPathing : MonoBehaviour {
+	NavMeshAgent agent;
+	public GameObject pathObject; //object that holds a path this car will follow
+	List<Transform> pathPoints; //transforms of a path defined by pathObject
+	int currentPoint; //current point # in its path
+	// Use this for initialization
+	void Start () {
+		pathPoints = pathObject.GetComponent<CarAIPath> ().path_objects;
+		agent = GetComponent<NavMeshAgent> ();
+		//agent.updatePosition = false;
+		//agent.updateRotation = false;
+		currentPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (agent.remainingDistance < 0.5f) {
+			if (currentPoint + 1 == pathPoints.Count) {
+				currentPoint = 0;
+			} else {
+				currentPoint++;
+			}
+			agent.SetDestination (pathPoints [currentPoint].position);
+		}
+		agent.nextPosition = transform.position;
+	}
+}
