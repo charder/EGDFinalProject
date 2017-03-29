d34efc77c8bdcbaf9b23f3b728855ff89bc79638
charder <chardergaming@gmail.com>
2017-03-28 23:37:34 -0400

    Trending topics are attached to cars.
    They don't rotate properly yet. I'll look at it in the morning... I'M BEAT

--- Assets/Scripts/Pathing/CarAIPathing.cs
+++ Assets/Scripts/Pathing/CarAIPathing.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class CarAIPathing : MonoBehaviour {
 	NavMeshAgent agent;
 	public GameObject pathObject; //object that holds a path this car will follow
+	public TextMesh hashtag; //this car's hashtag/trending topic
+	public GameObject playerCam; //find the camera
 	List<Transform> pathPoints; //transforms of a path defined by pathObject
 	public int currentPoint; //current point # in its path
 	// Use this for initialization
+
+	void Awake () {
+		hashtag = GetComponentInChildren<TextMesh> ();
+		playerCam = FindObjectOfType<AudioListener> ().gameObject;
+	}
+
 	void Start () {
 		pathPoints = pathObject.GetComponent<CarAIPath> ().path_objects;
 		agent = GetComponent<NavMeshAgent> ();
+		agent.SetDestination (pathPoints [currentPoint].position);
 		//agent.updatePosition = false;
 		//agent.updateRotation = false;
 	}
@@ -27,5 +37,7 @@ public class CarAIPathing : MonoBehaviour {
 			agent.SetDestination (pathPoints [currentPoint].position);
 		}
 		agent.nextPosition = transform.position;
+		//hashtag.gameObject.transform.LookAt (playerCam.transform);
+		print (playerCam.transform.position);
 	}
 }
