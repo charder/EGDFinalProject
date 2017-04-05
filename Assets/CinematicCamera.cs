3efbe2895b83a1890bfa62165e7cad4af9924cde
charder <chardergaming@gmail.com>
2017-04-04 22:47:44 -0400

    Version I recorded stuff with. Trailer is basically done.
    Recorded in a cinematic 24 frames per second... Just kidding, that's gross.

---
+++ Assets/CinematicCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicCamera : MonoBehaviour {
+	bool moving = false;
+	public Vector3 direction; //which direction to move the camera
+	public float speed; //speed to move camera at (in world space coordinates per second)
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Pause/Resume
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (moving) {
+				moving = false;
+			} else {
+				moving = true;
+			}
+		}
+		if (moving) {
+			transform.position = transform.position + direction * speed * Time.deltaTime;
+		}
+	}
+}
