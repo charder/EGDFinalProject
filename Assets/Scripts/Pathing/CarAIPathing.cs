181bb25e7e74542731265f31bc0e848fa79dbef8
charder <chardergaming@gmail.com>
2017-04-08 13:30:58 -0400

    HALLELUJAH! GOD This was awful.
    Cars pre-spawn now. Everything that could go wrong WAS going wrong though.

--- Assets/Scripts/Pathing/CarAIPathing.cs
+++ Assets/Scripts/Pathing/CarAIPathing.cs
@@ -11,35 +11,52 @@ public class CarAIPathing : MonoBehaviour {
 	public Transform playerCam; //find the camera
 	List<Transform> pathPoints; //transforms of a path defined by pathObject
 	public int currentPoint; //current point # in its path
-	// Use this for initialization
+	float reactionDelay = 2f; //time to wait until movement begins
+	public GameObject spawnPointPrefab;
 
 	void Awake () {
 		hashtag = GetComponentInChildren<TextMesh> ();
 		hashtag.transform.localScale = new Vector3 (-hashtag.transform.localScale.x, hashtag.transform.localScale.y, hashtag.transform.localScale.z);
 		playerCam = Camera.main.transform;
+		agent = GetComponent<NavMeshAgent> ();
 	}
 
 	void Start () {
+		//pathObject = pathObject.GetComponent<carSpawnPoint> ().pathToFollow;
+		agent.Warp(transform.position);
 		pathPoints = pathObject.GetComponent<CarAIPath> ().path_objects;
-		agent = GetComponent<NavMeshAgent> ();
 		agent.SetDestination (pathPoints [currentPoint].position);
-		//agent.updatePosition = false;
-		//agent.updateRotation = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (agent.remainingDistance < 8f) {
-			if (currentPoint + 1 == pathPoints.Count) {
-				currentPoint = 0;
-			} else {
-				currentPoint++;
-			}
-			agent.SetDestination (pathPoints [currentPoint].position);
+		if (reactionDelay > 0) {
+			reactionDelay -= Time.deltaTime;
 		}
-		//agent.nextPosition = transform.position;
-		hashtag.transform.LookAt(playerCam.position);
-		Vector3 hRot = hashtag.transform.rotation.eulerAngles;
-		//hashtag.transform.rotation = Quaternion.Euler (hRot.x, 0, hRot.z);
+		else {
+			if (agent.remainingDistance < 8f) {
+				if (currentPoint + 1 == pathPoints.Count) {
+					currentPoint = 0;
+				} else {
+					currentPoint++;
+				}
+				agent.SetDestination (pathPoints [currentPoint].position);
+			}
+			//agent.nextPosition = transform.position;
+			hashtag.transform.LookAt(playerCam.position);
+			Vector3 hRot = hashtag.transform.rotation.eulerAngles;
+			//hashtag.transform.rotation = Quaternion.Euler (hRot.x, 0, hRot.z);
+			//Press P to capture a paused version of the cars
+			if (Input.GetKeyDown(KeyCode.P)) {
+				GameObject carPoint = (GameObject)Instantiate (spawnPointPrefab, transform.position, transform.rotation);
+				carSpawnPoint carPointComp = carPoint.GetComponent<carSpawnPoint> ();
+				carPointComp.startNode = currentPoint;
+				carPointComp.pathToFollow = pathObject;
+			}
+		}
+	}
+
+	public void SetPathObject(GameObject obj) {
+		pathObject = obj;
 	}
 }
--- Assets/Scripts/Pathing/carSpawnPoint.cs
+++ Assets/Scripts/Pathing/carSpawnPoint.cs
@@ -1,28 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class CarSpawnOnPath : MonoBehaviour {
+public class carSpawnPoint : MonoBehaviour {
 	public GameObject pathToFollow; //path of nodes the car will follow
 	public GameObject[] carPrefabs; //which car prefab to spawn
 	public Material[] carMaterials;
 	public int startNode; //node on the path the spawned car will start on
-	public float startDelay; //delay on spawning the first car
-	public float spawnInterval; //time between car spawns after first spawn
-	public float spawnTime; //how long to spawn for before stopping
-	float timePassed = 0;
-
-	bool spawnStarted; //whether the script has started spawning
-	private float timeTrack; 
-	bool stop = false;
 	TwitterHandler twitHandler; //reference to the twitter handler script
+	float delaySpawn = 2f;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		twitHandler = FindObjectOfType<TwitterHandler> ();
-		spawnStarted = false;
-		startDelay *= -1; //switch to negative value for countdown
-		timeTrack = 0;
+		//spawnCar ();
 	}
 
 	void OnDrawGizmos() {
@@ -31,29 +21,18 @@ public class CarSpawnOnPath : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (timePassed < spawnTime) {
-			timePassed += Time.deltaTime;
-			if (timePassed >= spawnTime) {
-				stop = true;
-			}
-		}
-		if (!stop) {
-			if (!spawnStarted && timeTrack <= startDelay) {
-				spawnStarted = true;
-				timeTrack = spawnInterval;
-				spawnCar ();
-			} else if (spawnStarted && timeTrack <= 0) {
-				timeTrack = spawnInterval;
+		if (delaySpawn > 0) {
+			delaySpawn -= Time.deltaTime;
+			if (delaySpawn <= 0) {
 				spawnCar ();
 			}
-			timeTrack -= Time.deltaTime;
 		}
 	}
 
 	void spawnCar() {
 		int roll = Random.Range (0, carPrefabs.Length);
 		int rollC = Random.Range (0, carMaterials.Length);
-		GameObject tmp = (GameObject)Instantiate (carPrefabs[roll],transform.position,Quaternion.identity);
+		GameObject tmp = (GameObject)Instantiate (carPrefabs [roll], transform.position, transform.rotation);
 		CarAIPathing carTmp = tmp.GetComponent<CarAIPathing> ();
 		//tmp.GetComponentInChildren<MeshRenderer> ().materials [0] = carMaterials [rollC];
 		MeshRenderer carMesh = tmp.GetComponentInChildren<MeshRenderer>();
@@ -68,5 +47,6 @@ public class CarSpawnOnPath : MonoBehaviour {
 		int rollT = Random.Range (0, twitHandler.trendKeys.Count); //roll for trending topic to put on car <<<<<<< TEMPORARY?
 		tmp.GetComponentInChildren<TextMesh>().text = twitHandler.trendKeys[rollT];
 		carTmp.SetPathObject (pathToFollow);
+		carTmp.currentPoint = startNode;
 	}
 }
