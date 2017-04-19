321e3fe0ceda66469ce36da0422bc9f145303334
Stephan Jiang <stephen665021@gmail.com>
2017-04-19 15:57:58 -0400

    Added the little "Press Start" thing & fade out when entering main scene.

--- Assets/Art/MainMenuAsset/MainMenuCameraScript.cs
+++ Assets/Art/MainMenuAsset/MainMenuCameraScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenuCameraScript : MonoBehaviour {
@@ -7,10 +8,56 @@ public class MainMenuCameraScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         CameraAnimator = GetComponent<Animator>();
+        UpdatingAlpha = 0.0f;
+        bFadeOut = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        // The fade in fade out for "press start"
+        if (UpdatingAlpha < 1.0f && !bFadeOut)
+        {
+            UpdatingAlpha += Time.deltaTime;
+        }
+        if (UpdatingAlpha > 0.0f && bFadeOut)
+        {
+            UpdatingAlpha -= Time.deltaTime;
+        }
+        if (!bEnteringNewScene)
+        {
+            if (UpdatingAlpha >= 1.0f)
+            {
+                bFadeOut = true;
+            }
+            if (UpdatingAlpha <= 0.0f)
+            {
+                bFadeOut = false;
+            }
+        }
+
+        if(bEnteringNewScene)
+        {
+            FadeOutImage.color = new Color(0.0f, 0.0f, 0.0f, UpdatingAlpha);
+        }
+
+        // Camera animations
+        if(CameraAnimator.GetBool("IsZoomed"))
+        {
+            StartText.color = new Color(255.0f, 255.0f, 255.0f, 0.0f);
+        }
+        else
+        {
+            if (CameraAnimator.GetBool("ShouldZoom"))
+            {
+                StartText.color = new Color(255.0f, 255.0f, 255.0f, 0.0f);
+            }
+            else
+            {
+                StartText.color = new Color(255.0f, 255.0f, 255.0f, UpdatingAlpha);
+            }
+        }
+
         if (Input.GetKeyDown("t"))
         {
             CameraAnimator.SetBool("ShouldZoom", !CameraAnimator.GetBool("ShouldZoom"));
@@ -31,7 +78,22 @@ public class MainMenuCameraScript : MonoBehaviour {
                 CameraAnimator.SetBool("IsZoomed", false);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UpdatingAlpha = 0.01f;
+            FadeOutImage.color = new Color(0.0f, 0.0f, 0.0f, UpdatingAlpha);
+            bEnteringNewScene = true;
+            bFadeOut = false;
+        }
     }
+
+    public Text StartText;
+    public Image FadeOutImage;
+
+    private bool bEnteringNewScene;
+    private bool bFadeOut;
+    private float UpdatingAlpha;
 
     private Animator CameraAnimator;
 }
