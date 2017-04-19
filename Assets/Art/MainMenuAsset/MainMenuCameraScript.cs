515348162c9f70753de0e381844da4f5403c6c3e
Stephan Jiang <stephen665021@gmail.com>
2017-04-19 14:35:04 -0400

    Basic camera transition for main menu ("press "t" to zoom for entering twitter account")

---
+++ Assets/Art/MainMenuAsset/MainMenuCameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+        bWait = false;
+        CameraAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("t"))
+        {
+            bWait = true;
+
+            CameraAnimator.SetBool("ShouldZoom", !CameraAnimator.GetBool("ShouldZoom"));
+        }
+
+        if (bWait)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer>0.5f)
+        {
+            CameraAnimator.SetBool("IsZoomed", !CameraAnimator.GetBool("IsZoomed"));
+            bWait = false;
+            timer = 0.0f;
+        }
+
+	}
+
+    private float timer;
+    private bool bWait;
+    private Animator CameraAnimator;
+}
